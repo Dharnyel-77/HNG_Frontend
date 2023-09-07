adca8995f6ac3e75582d1e19fd010cd08c4de523
ohiozedaniel8@gmail.com <Daniel>
2023-09-07 11:45:31 +0100

    Updated committed

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const currentTime = new Date().getTime();
 const UTCTime = document.querySelector('#time');
 
 UTCTime.innerHTML = currentTime;
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = new Date();
+const dayOfWeek = today.getDay();
+
+const currentDay = document.querySelector('.day');
+
+const currentDayOfWeek = daysOfWeek[dayOfWeek];
+currentDay.innerHTML = currentDayOfWeek;
+console.log(currentDayOfWeek);
