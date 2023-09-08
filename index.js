4cb2ae1b53c7a42ffff655a93502561fb3aa21bc
ohiozedaniel8@gmail.com <Daniel>
2023-09-08 11:31:33 +0100

    further updates

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
 
-const currentTime = new Date().getTime();
-const UTCTime = document.querySelector('#time');
+function updateTime() {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours().toString().padStart(2, '0');
+    const currentMinute = currentDate.getMinutes().toString().padStart(2, '0');
+    const currentSecond = currentDate.getSeconds().toString().padStart(2, '0');
+  
+    const currentTimeString = `${currentHour}:${currentMinute}:${currentSecond}`;
+    
+    document.getElementById('time').textContent = currentTimeString;
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
+  
 
-UTCTime.innerHTML = currentTime;
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const today = new Date();
